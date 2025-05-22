c6b533e1ad3bd3f3be0375922688d093b2da480f
Roney Vila <129080415+akirar0n@users.noreply.github.com>
2025-05-22 14:49:14 -0300

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -3,45 +3,23 @@ use berethdb;
 
 create table usuario(
 	id_usuario int auto_increment,
+	acesso boolean not null default false,
+	email varchar(80) not null,
+	senha varchar(100) not null,
 	cpf varchar(11) not null,
 	rg varchar(11)not null,
 	nome varchar(255)not null,
-	dataRegistro datetime default CURRENT_TIMESTAMP() not null,
+	telefone varchar(16),
 	idade int,
-	nascimento date not null,
-		primary key(id_usuario)
-);
-
-create table usuarioTelefone(
-	id_telefone int auto_increment,
-	id_usuario int,
-	telefone varchar(16), -- conseiderando formatação exemplo:(61) 91234-5678
-		primary key(id_telefone)
-);
-
-create table usuarioLogin (
-	id_login int auto_increment,
-	id_usuario int, -- usuario(id_usuario)
-	acesso boolean not null default false,-- ("admin","cliente"),
--- 	id_telefone // possível implementação de método de recuperação de conta por telefone
-	email varchar(80) not null,
-	senha varchar(100) not null,
-	dataRegistro datetime default current_timestamp() not null,
-		primary key(id_login),
-		foreign key(id_usuario) references usuario(id_usuario)
-);
-
-create table usuarioEndereco (
-	id_endereco INT auto_increment,
-	id_usuario INT,
 	numero INT not null,
 	bairro VARCHAR(50) not null,
 	cidade VARCHAR(50) not null,
 	estado VARCHAR(50) not null,
 	complemento VARCHAR(255),
 	cep VARCHAR(11) not null,
-		primary key(id_endereco),
-		foreign key(id_usuario) references usuario(id_usuario)
+	nascimento date not null,
+	dataRegistro datetime default CURRENT_TIMESTAMP() not null,
+		primary key(id_usuario)
 );
 
 create table veiculo(
