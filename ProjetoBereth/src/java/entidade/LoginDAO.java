3328ce74f7f9d7103d91faeabd1b6f773812a4df
null <bryan.jvo@gmail.com>
2025-05-23 10:55:34 -0300

    sistema de login e muitas outras coisas

--- ProjetoBereth/src/java/entidade/LoginDAO.java
+++ ProjetoBereth/src/java/entidade/LoginDAO.java
@@ -6,20 +6,24 @@
 package entidade;
 
 import controle.UsuarioLogin;
+import controle.Usuarios;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import org.mindrot.jbcrypt.BCrypt;
 
 public class LoginDAO extends DAO {
 
-    public boolean inserir(UsuarioLogin login) {
+    public boolean inserir(Usuarios login) {
     try {
         abrirBanco();
         String senhaCriptografada = BCrypt.hashpw(login.getSenha(), BCrypt.gensalt());
-        String query = "INSERT INTO usuarioLogin (email, senha) VALUES(?, ?)";
+        String query = "INSERT INTO usuario (email, senha, cpf, nome, endereco) VALUES(?, ?, ?, ?, ?)";
         pst = con.prepareStatement(query);
         pst.setString(1, login.getEmail());
         pst.setString(2, senhaCriptografada);
+        pst.setString(3, login.getCpf());
+        pst.setString(4, login.getNome());
+        pst.setString(5, login.getEndereco());
 
 
         int linhasAfetadas = pst.executeUpdate(); // Retorna número de linhas afetadas
@@ -32,12 +36,12 @@ public class LoginDAO extends DAO {
     }
 }
 
-    public UsuarioLogin pesquisar(UsuarioLogin login) {
-        UsuarioLogin loginBuscado = new UsuarioLogin();
+    public Usuarios pesquisar(Usuarios login) {
+        Usuarios loginBuscado = new Usuarios();
 
         try {
             abrirBanco();
-            String query = "SELECT id, nome, email, senha, endereco FROM clientes WHERE email = ?";
+            String query = "SELECT id_usuario, nome, email, senha, endereco FROM usuario WHERE email = ?";
             pst = con.prepareStatement(query);
             pst.setString(1, login.getEmail());
 
@@ -48,10 +52,10 @@ public class LoginDAO extends DAO {
 
                 // Verificar se a senha informada corresponde ao hash armazenado
                 if (BCrypt.checkpw(login.getSenha(), senhaHashBanco)) {
-                    loginBuscado = new UsuarioLogin();
-                    loginBuscado.setId_login(rs.getInt("id"));
+                    loginBuscado = new Usuarios();
+                    loginBuscado.setId_usuario(rs.getInt("id_usuario"));
                     loginBuscado.setEmail(rs.getString("email"));
-                    loginBuscado.setSenha(rs.getString("endereco"));
+                    loginBuscado.setSenha(rs.getString("senha"));
                     // você pode adicionar mais campos se precisar
                 }
             }
