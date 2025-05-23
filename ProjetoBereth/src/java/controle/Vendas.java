d4185cdb5a31c103c64d0489dbd0a7c17557f955
ThiagoSilva <thiagoantonio2108@gmail.com>
2025-05-23 11:27:14 -0300

    implementação de mantervenda com classe e entidade

---
+++ ProjetoBereth/src/java/controle/Vendas.java
@@ -0,0 +1,80 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controle;
+
+import Enuns.FormaPagamento;
+import java.sql.Timestamp;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Vendas {
+    private int id_venda;
+    private int id_usuario;
+    private int id_veiculo;
+    private FormaPagamento formaPagamento;
+    private int parcelas;
+    private float valor;
+    private Timestamp dataVenda;
+
+    public int getId_venda() {
+        return id_venda;
+    }
+
+    public void setId_venda(int id_venda) {
+        this.id_venda = id_venda;
+    }
+
+    public int getId_usuario() {
+        return id_usuario;
+    }
+
+    public void setId_usuario(int id_usuario) {
+        this.id_usuario = id_usuario;
+    }
+
+    public int getId_veiculo() {
+        return id_veiculo;
+    }
+
+    public void setId_veiculo(int id_veiculo) {
+        this.id_veiculo = id_veiculo;
+    }
+
+    public FormaPagamento getFormaPagamento() {
+        return formaPagamento;
+    }
+
+    public void setFormaPagamento(FormaPagamento formaPagamento) {
+        this.formaPagamento = formaPagamento;
+    }
+
+    public int getParcelas() {
+        return parcelas;
+    }
+
+    public void setParcelas(int parcelas) {
+        this.parcelas = parcelas;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    public Timestamp getDataVenda() {
+        return dataVenda;
+    }
+
+    public void setDataVenda(Timestamp dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    
+}
