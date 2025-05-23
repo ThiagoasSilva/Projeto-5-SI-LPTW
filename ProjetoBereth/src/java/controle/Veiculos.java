9faa277030d13fb956c810528d1b37b6410bd9b6
ThiagoSilva <thiagoantoino2108@gmail.com>
2025-05-23 10:54:12 -0300

    update veiculos

---
+++ ProjetoBereth/src/java/controle/Veiculos.java
@@ -0,0 +1,147 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controle;
+
+/**
+ *
+ * @author Roney
+ */
+public class Veiculos {
+    private int id_veiculo;
+    private String categoria;
+    private String tipo;
+    private String marca;
+    private String modelo;
+    private String cor;
+    private int rodas;
+    private float motorizacao;
+    private float peso;
+    private float capTanque;
+    private int assentos;
+    private int anoFabr;
+    private int anoModelo;
+    private String placa;
+    private String chassi;
+
+    public int getId_veiculo() {
+        return id_veiculo;
+    }
+
+    public void setId_veiculo(int id_veiculo) {
+        this.id_veiculo = id_veiculo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public int getRodas() {
+        return rodas;
+    }
+
+    public void setRodas(int rodas) {
+        this.rodas = rodas;
+    }
+
+    public float getMotorizacao() {
+        return motorizacao;
+    }
+
+    public void setMotorizacao(float motorizacao) {
+        this.motorizacao = motorizacao;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public float getCapTanque() {
+        return capTanque;
+    }
+
+    public void setCapTanque(float capTanque) {
+        this.capTanque = capTanque;
+    }
+
+    public int getAssentos() {
+        return assentos;
+    }
+
+    public void setAssentos(int assentos) {
+        this.assentos = assentos;
+    }
+
+    public int getAnoFabr() {
+        return anoFabr;
+    }
+
+    public void setAnoFabr(int anoFabr) {
+        this.anoFabr = anoFabr;
+    }
+
+    public int getAnoModelo() {
+        return anoModelo;
+    }
+
+    public void setAnoModelo(int anoModelo) {
+        this.anoModelo = anoModelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+}
